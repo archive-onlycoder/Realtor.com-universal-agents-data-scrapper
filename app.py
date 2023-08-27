58e00015737447d39cac9841487d5e35eab39096
onlycoder000 <only.coder000@gmail.com>
2023-08-27 13:26:04 +0600

    v.2

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ chrome_options = Options()
 chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 driver = webdriver.Chrome(options=chrome_options)
 driver.get('https://www.realtor.com/')
-
+inp=input('Press Enter to start')
 # %%
 # main code
 
